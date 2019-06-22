b132aee1948741bbfbbbac03206707be3dea60bc
Tyler Reece <tyler_reece@outlook.com>
2019-06-22 16:08:54 -0400

    Formatting and clean up

--- lib/exercise/ExerciseHome.dart
+++ lib/exercise/ExerciseHome.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:workout_tracking_essentials/genericWidgets/MyCard.dart';
 
 class ExerciseHome extends StatelessWidget {
   ExerciseHome({this.style}) : super();
@@ -8,72 +7,9 @@ class ExerciseHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GridView.count(
-        primary: true,
-        padding: const EdgeInsets.all(1.0),
-        crossAxisCount: 2,
-        childAspectRatio: 0.85,
-        mainAxisSpacing: 1.0,
-        crossAxisSpacing: 1.0,
-        children: <Widget>[
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-          new MyCard(
-              // Setup the text
-              title: new Text(
-                "Favorite",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              // Setup the icon
-              icon: new Icon(Icons.favorite, size: 10, color: Colors.red)),
-        ],
-      ),
+          body: Center(
+            child: Text('Exercise', style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold)),
+          )
     );
   }
 }
--- lib/genericWidgets/ExerciseCard.dart
+++ lib/genericWidgets/ExerciseCard.dart
@@ -12,7 +12,8 @@ class ExerciseCard extends StatelessWidget {
     return workouts
         .map((workout) => new Row(
               children: <Widget>[
-                Padding(padding: EdgeInsets.only(left: 10.0), child: Text(workout)),
+                Padding(
+                    padding: EdgeInsets.only(left: 10.0), child: Text(workout)),
               ],
             ))
         .toList();
@@ -24,39 +25,42 @@ class ExerciseCard extends StatelessWidget {
         padding: EdgeInsets.all(5.0),
         child: Container(
             child: Card(
-          color: Theme.of(context).cardColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  Padding(
-                      padding: EdgeInsets.only(top: 3.0, bottom: 3.0),
-                      child: Text(title,
-                          style: TextStyle(
-                              fontSize: 20.0, fontWeight: FontWeight.bold))),
-                ],
-              ),
-              ...workoutList(),
-              Padding(
-                padding: EdgeInsets.only(left: 8.0, right: 8.0),
-                child: RaisedButton(
-                    onPressed: () => {},
-                    elevation: 10.0,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text('Start Workout'),
-                        Icon(icon),
-                      ],
-                    )),
-              ),
-            ],
-          ),
-        )));
+                color: Theme.of(context).cardColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: InkWell(
+                  onTap: () => {},
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          Padding(
+                              padding: EdgeInsets.only(top: 3.0, bottom: 3.0),
+                              child: Text(title,
+                                  style: TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.bold))),
+                        ],
+                      ),
+                      ...workoutList(),
+                      Padding(
+                        padding: EdgeInsets.only(left: 8.0, right: 8.0),
+                        child: RaisedButton(
+                            onPressed: () => {},
+                            elevation: 10.0,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text('Start Workout'),
+                                Icon(icon),
+                              ],
+                            )),
+                      ),
+                    ],
+                  ),
+                ))));
   }
 }
