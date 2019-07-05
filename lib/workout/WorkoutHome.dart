6a0797bf4849e3ccd85e11d0be404cd2cf3c5a6e
Tyler Reece <tyler_reece@outlook.com>
2019-07-05 08:58:46 -0400

    Returned List<Widget> from FutureBuilder

--- lib/workout/WorkoutHome.dart
+++ lib/workout/WorkoutHome.dart
@@ -4,80 +4,96 @@ import 'package:flutter/material.dart';
 import 'package:workout_tracking_essentials/exercise/CreateRoutine.dart';
 import 'package:workout_tracking_essentials/exercise/RoutineCard.dart';
 import 'package:workout_tracking_essentials/model/Routine.dart';
+import 'package:workout_tracking_essentials/model/User.dart';
 
 import 'MyRoutines.dart';
 
 class WorkoutHome extends StatelessWidget {
   Routine routine;
 
-  Future<Routine> _getRoutine(context) async {
-    final routineString = await DefaultAssetBundle.of(context)
-        .loadString('assets/routines.json');
-    final map = json.decode(routineString);
-    Routine loadedRoutine = Routine.fromJson(map);
+  Future<List<Routine>> _getRoutines(context) async {
+    String routineString =
+        await DefaultAssetBundle.of(context).loadString('assets/routines.json');
+    Map<String, dynamic> jsonMap = await json.decode(routineString);
+    User user = User.fromJson(jsonMap);
 
-    return loadedRoutine;
+    return user.routines;
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Container(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Padding(
-                          padding: EdgeInsets.all(10.0),
-                          child: Text(
-                            'Workout',
-                            style: Theme.of(context).textTheme.title,
+      child: FutureBuilder(
+        future: _getRoutines(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return CustomScrollView(
+              slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              Padding(
+                                padding: EdgeInsets.all(10.0),
+                                child: Text(
+                                  'Workout',
+                                  style: Theme.of(context).textTheme.title,
+                                ),
+                              ),
+                            ],
                           ),
+                          Column(
+                            children: <Widget>[],
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(
+                          'Routines',
+                          style: Theme.of(context).textTheme.subtitle,
                         ),
-                      ],
-                    ),
-                    Column(
-                      children: <Widget>[
-                      ],
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Text(
-                    'Routines',
-                    style: Theme.of(context).textTheme.subtitle,
+                      ),
+                      Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: RaisedButton(
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => CreateRoutine())),
+                            child: Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                            color: Colors.green,
+                          ))
+                    ],
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => CreateRoutine())),
-                      child: Icon(Icons.add, color: Colors.white,),
-                      color: Colors.green,
-                    )
-                )
-              ],
-            ),
-          ),
-          MyRoutines(),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text('Example Routines',
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-              ),
-            ]),
-          ),
+                SliverGrid(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  delegate: SliverChildListDelegate([
+                    ...snapshot.data
+                        .map<Widget>((routine) => RoutineCard(routine))
+                        .toList()
+                  ]),
+                ),
+                SliverList(
+                  delegate: SliverChildListDelegate([
+                    Padding(
+                      padding: EdgeInsets.all(10.0),
+                      child: Text('Example Routines',
+                          style: TextStyle(
+                              fontSize: 20, fontWeight: FontWeight.bold)),
+                    ),
+                  ]),
+                ),
 //          SliverGrid(
 //            gridDelegate:
 //                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
@@ -94,7 +110,14 @@ class WorkoutHome extends StatelessWidget {
 //              },),
 //            ]),
 //          ),
-        ],
+              ],
+            );
+          } else if (snapshot.hasError) {
+            print(snapshot.error);
+            return Text("${snapshot.error}");
+          }
+          return CircularProgressIndicator();
+        },
       ),
     ));
   }
